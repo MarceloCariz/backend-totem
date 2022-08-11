723e4ec89b5c9aa122db7d78cf25f8ff88fc86a8
Francisco Uribe <franciscouribematus@gmail.com>
2022-08-10 23:34:50 -0400

    modelos alumno y docente

--- src/models/Docente.js
+++ src/models/Docente.js
@@ -1,66 +1,10 @@
 const { Schema, model } = require('mongoose');
 
 const Docente = new Schema({
-    Rut_Alumno:{
-        type: String
-    },
-    Codigo_Seccion:{
-        type: String
-    },
-    Nombre_Asignatura:{
-        type: String
-    },
-    Nombre_Alumno:{
-        type: String
-    },
-    Apellido_Paterno_Alumno:{
-        type: String
-    },
-    Apellido_Materno_Alumno: {
-        type: String
-    },
-    Correo_Alumno:{
-        type: String
-    },
-    Telefono: {
-        type: String
-    },
-    Codigo_carrera: {
-        type: String
-    },
-    Carrera: {
-        type: String
-    },
-    Jornada: {
-        type: String
-    },
-    Id_Docente: {
-        type: String
-    },
-    Nombre_Docente: {
-        type: String
-    },
-    Apellido_Paterno_Docente: {
-        type: String
-    },
-    Apellido_Materno_Docente: {
-        type: String
-    },
-    Correo_Docente: {
-        type: String
-    },
-    Nombre_Coordinador: {
-        type: String
-    },
-    Apellido_Paterno_Coordinador: {
-        type: String
-    },
-    Apellido_Materno_Coordinador: {
-        type: String
-    },
-    Correo_Coordinador: {
-        type: String
-    }
+    nombre_seccion: {type: String},
+    Nombre_Asignatura: {type: String},
+    rut_docente: {type: String},
+    Aula: {type: String}
 })
 
-module.exports = model( 'docentes', Alumno );
+module.exports = model( 'docentes', Docente );
